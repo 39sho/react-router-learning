f965e8db70d8decd1b0cf58aff8b39e0a7182815
39sho <g.39sho@gmail.com>
2023-05-29 18:37:05 +0900

    add router

---
+++ src/Root.tsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+export default () => {
+    return (
+        <>
+            <span>Here is Root</span>
+            <Link to="/team">team</Link>
+        </>);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from './Root.jsx';
+import Team from './Team.jsx';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "team",
+    element: <Team />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
